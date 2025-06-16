af5928744febcbca3e4ffe1aa6c327d489e7b248
Sang Virgo <tansang06092004@gmail.com>
2025-06-16 13:21:45 +0700

    fix(project): add vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_BASE_URL;
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const API_URL = `${BACKEND_URL}/api/v1`;
 
 const api = axios.create({
     baseURL: API_URL,
