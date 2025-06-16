99e2875dee131571718a77dc058b312d3f0f8469
Sang Virgo <tansang06092004@gmail.com>
2025-06-16 13:29:06 +0700

    fix(project): add api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
+const BACKEND_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 const API_URL = `${BACKEND_URL}/api/v1`;
 
 const api = axios.create({
